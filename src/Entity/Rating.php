f1ab676d241b130ce18bc23a0042367fc1267e4c
Diego Avola <Diego.Avola@metronetinc.com>
2022-06-30 21:16:46 -0300

    New Entities

---
+++ src/Entity/Rating.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RatingRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=RatingRepository::class)
+ */
+class Rating
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $value;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Movie::class, inversedBy="ratings")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Movie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getValue(): ?float
+    {
+        return $this->value;
+    }
+
+    public function setValue(float $value): self
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    public function getMovie(): ?Movie
+    {
+        return $this->Movie;
+    }
+
+    public function setMovie(?Movie $Movie): self
+    {
+        $this->Movie = $Movie;
+
+        return $this;
+    }
+}
