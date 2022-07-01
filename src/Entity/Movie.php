f6bca3ebb5c01af243fe6b99491a9baae341c51e
Diego Avola <Diego.Avola@metronetinc.com>
2022-07-01 00:16:20 -0300

    Only GET, POST, GET[] endopints

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -9,7 +9,10 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     collectionOperations={"get","post"},
+ *     itemOperations={"get"}
+ * )
  * @ORM\Entity(repositoryClass=MovieRepository::class)
  */
 class Movie
