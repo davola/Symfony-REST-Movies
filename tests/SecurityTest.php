afb67ee3fd33fd39d347da8f540a797c23709ac5
Diego Avola <Diego.Avola@metronetinc.com>
2022-07-06 20:09:48 -0300

    Last etst details

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -23,12 +23,6 @@ class SecurityTest extends AbstractTest
         $this->assertCount(2, $results);
     }
 
-    public function test_user_3_only_lists_movies_he_owns(): void
-    {
-        $results = $this->assertLoggedInUserIsMoviesOwner('user3@example.com', 'user3');
-        $this->assertCount(3, $results);
-    }
-
     public function test_user_1_only_reads_movies_he_owns(): void
     {
         $client = $this->getClientForCredentials('user1@example.com', 'user1');
@@ -41,13 +35,6 @@ class SecurityTest extends AbstractTest
         $client = $this->getClientForCredentials('user2@example.com', 'user2');
         $this->assertUserReadsMovieHeOwns($client, self::USER_2_MOVIES);
         $this->assertUserReadsNotOthersMovies($client, array_diff(self::USER_2_MOVIES, self::ALL_MOVIES));
-    }
-
-    public function test_user_3_only_reads_movies_he_owns(): void
-    {
-        $client = $this->getClientForCredentials('user3@example.com', 'user3');
-        $this->assertUserReadsMovieHeOwns($client, self::USER_3_MOVIES);
-        $this->assertUserReadsNotOthersMovies($client, array_diff(self::USER_3_MOVIES, self::ALL_MOVIES));
     }
 
     private function assertLoggedInUserIsMoviesOwner($username, $password): array
