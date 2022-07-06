f643a136470e067bf913206eea56b2f587fd535c
Diego Avola <Diego.Avola@metronetinc.com>
2022-07-06 02:20:31 -0300

    Movies test constants

--- tests/SecurityTest.php
+++ tests/SecurityTest.php
@@ -6,6 +6,11 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SecurityTest extends AbstractTest
 {
+    private const USER_1_MOVIES = [1];
+    private const USER_2_MOVIES = [2,3];
+    private const USER_3_MOVIES = [4,5,6];
+    private const ALL_MOVIES = [1,2,3,4,5,6];
+
     public function test_user_1_only_lists_movies_he_owns(): void
     {
         $results = $this->assertUserPassIsOwner('user1@example.com', 'user1');
@@ -27,22 +32,22 @@ class SecurityTest extends AbstractTest
     public function test_user_1_only_reads_movies_he_owns(): void
     {
         $client = $this->getClientForCredentials('user1@example.com', 'user1');
-        $this->assertUserReadsMovieHeOwns($client, [1]);
-        $this->assertUserDoesNotReadsOthersMovies($client, [2,3,4,5,6]);
+        $this->assertUserReadsMovieHeOwns($client, self::USER_1_MOVIES);
+        $this->assertUserDoesNotReadsOthersMovies($client, array_diff(self::USER_1_MOVIES, self::ALL_MOVIES));
     }
 
     public function test_user_2_only_reads_movies_he_owns(): void
     {
         $client = $this->getClientForCredentials('user2@example.com', 'user2');
-        $this->assertUserReadsMovieHeOwns($client, [2, 3]);
-        $this->assertUserDoesNotReadsOthersMovies($client, [1, 4, 5, 6]);
+        $this->assertUserReadsMovieHeOwns($client, self::USER_2_MOVIES);
+        $this->assertUserDoesNotReadsOthersMovies($client, array_diff(self::USER_2_MOVIES, self::ALL_MOVIES));
     }
 
     public function test_user_3_only_reads_movies_he_owns(): void
     {
         $client = $this->getClientForCredentials('user3@example.com', 'user3');
-        $this->assertUserReadsMovieHeOwns($client, [4,5,6]);
-        $this->assertUserDoesNotReadsOthersMovies($client, [1,2,3]);
+        $this->assertUserReadsMovieHeOwns($client, self::USER_3_MOVIES);
+        $this->assertUserDoesNotReadsOthersMovies($client, array_diff(self::USER_3_MOVIES, self::ALL_MOVIES));
     }
 
     private function assertUserPassIsOwner($username, $password): array
