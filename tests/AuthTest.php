1e076a5c8d1cc7ddaafdda8d952a5289fbacd2d9
Diego Avola <Diego.Avola@metronetinc.com>
2022-07-06 23:03:12 -0300

    Final missing tests

--- tests/AuthTest.php
+++ tests/AuthTest.php
@@ -6,7 +6,6 @@ class AuthTest extends AbstractTest
 {
     public function test_login_check_token_success(): void
     {
-        // TODO - add full assertions
         $response = $this->getClient()->request('POST', '/api/login_check', ['json' => [
             'username' => 'user1@example.com',
             'password' => 'user1',
@@ -18,7 +17,37 @@ class AuthTest extends AbstractTest
 
     public function test_login_check_token_wrong_credentials()
     {
-        // TODO - add assertions
-        $this->assertTrue(true);
+        $this->getClient()->request('POST', '/api/login_check', ['json' => [
+            'username' => 'user1@example.com',
+            'password' => 'user2',
+        ]]);
+
+        $this->assertResponseStatusCodeSame(401);
+        $this->assertJsonContains([
+            'code' => 401,
+            'message' => 'Invalid credentials.',
+        ]);
+    }
+
+    public function test_no_authorization_cant_get_movies()
+    {
+        $this->getClient()->request('GET', '/api/v1/movies', []);
+
+        $this->assertResponseStatusCodeSame(401);
+        $this->assertJsonContains([
+            'code' => 401,
+            'message' => 'JWT Token not found',
+        ]);
+    }
+
+    public function test_no_authorization_cant_post_movies()
+    {
+        $this->getClient()->request('POST', '/api/v1/movies', ['json' => MoviesTest::MOVIE_ALIEN]);
+
+        $this->assertResponseStatusCodeSame(401);
+        $this->assertJsonContains([
+            'code' => 401,
+            'message' => 'JWT Token not found',
+        ]);
     }
 }
